9837399d7aac4d25afb5591b8521080ae52f42fa
uysolanki <uysolanki@gmail.com>
2026-02-13 12:38:30 +0530

    exception handling

--- src/day5/MultipleExceptionDemo.java
+++ src/day5/MultipleExceptionDemo.java
@@ -9,6 +9,17 @@ public class MultipleExceptionDemo {
 		Scanner sc=new Scanner(System.in);
 		try
 		{
+		String name="Alice";
+		name=null;
+		
+		System.out.println("Length of String is " +name.length());
+		
+		
+		int ar[]=new int[5];	//index 0,1,2,3,4    
+		
+		ar[5]=100;				//throw new ArrayIndexOutOfBoundsException
+		
+		
 		System.out.println("Enter Numerator");
 		int num=sc.nextInt();
 		System.out.println("Enter Denominator");
@@ -20,12 +31,23 @@ public class MultipleExceptionDemo {
 		}
 		catch(InputMismatchException e1)
 		{
-			e1.printStackTrace();
+			//e1.printStackTrace();   //logging
+			System.out.println("Please enter integer values only");
 		}
 		
 		catch (ArithmeticException e2)
 		{
 			System.out.println(e2.getMessage());
+		}
+		
+		catch (ArrayIndexOutOfBoundsException e3)
+		{
+			System.out.println(e3.getMessage());
+		}
+		
+		catch (Exception e4)		//default EH
+		{
+			System.out.println("Ref is not pointing to any object");
 		}
 
 
