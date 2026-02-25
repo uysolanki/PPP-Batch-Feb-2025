d312280ba81011a6fc524071ec6d6c7f4cfcd3a2
uysolanki <uysolanki@gmail.com>
2026-02-25 16:17:46 +0530

    file handling

---
+++ src/day13/TryWithResources.java
@@ -0,0 +1,31 @@
+package day13;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TryWithResources {
+public static void main(String[] args) {
+	String fileName="myfolder/file1.txt";
+	
+	try 
+	{
+		File file=new File(fileName);
+		if(file.exists())
+		{
+		FileWriter fw=new FileWriter(fileName,true);
+		fw.append(" my friends");
+		fw.close();
+		}
+		else
+		{
+			System.out.println("File does not exists");
+		}
+	} catch (IOException e) 
+	{
+		e.printStackTrace();
+	}
+	
+	//System.out.println("Data written to file");
+}
+}
