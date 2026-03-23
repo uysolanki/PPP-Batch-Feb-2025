9b417fc05af19df8a4e26df29b2b5e77b59ae138
uysolanki <uysolanki@gmail.com>
2026-03-23 14:50:15 +0530

    searching linear

--- src/day26/LinearSearchDemo5.java
+++ src/day26/LinearSearchDemo5.java
@@ -1,26 +1,34 @@
 package day26;
 
-public class LinearSearchDemo {
+public class LinearSearchDemo5 {
 
 	public static void main(String[] args) {
-				//  i
+				//            i           i
 		int arr[]= {8,2,9,3,5};
 		
-		int search=2;
-		int flag=0;
+		int ans=linearSearch(arr);
+		if(ans==-1)
+		{
+			System.out.println("NOT FOUND");
+		}
+		else
+		{
+			System.out.println("FOUND AT INDEX "+ans);
+		}
+	}
+
+	private static int linearSearch(int[] arr) {
+		int search=5;
 		for(int i=0;i<arr.length;i++)						//i						arr[i]					search flag 
 		{													//0		is 0<5 True		8		is 8==2 false	2      0
 			if(arr[i]==search)								//1     is 1<5 True	 	2       is 2==2 true           1
 			{
 				System.out.println("FOUND");															//is 1==0 false
-				flag=1;
-				break;
+				return i;
 			}
 		}
-		
-		if(flag==0)
-			System.out.println("NOT FOUND");
-
+			
+		return -1;
 	}
 
 }
---
+++ src/day26/LinearSearchDemo6.java
@@ -0,0 +1,21 @@
+package day26;
+
+public class LinearSearchDemo6 {
+
+	public static void main(String[] args) {
+				
+		int arr[]= {8,2,9,3,5};
+		
+		int ans=linearSearch(arr);
+		System.out.println(ans==-1?"NOT FOUND ": "FOUND AT INDEX "+ans);
+	}
+
+	private static int linearSearch(int[] arr) {
+		int search=5;
+		for(int i=0;i<arr.length;i++)					
+			if(arr[i]==search) return i;
+			
+		return -1;
+	}
+
+}
