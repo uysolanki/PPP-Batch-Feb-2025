a83069698815ea92919cb47f17fff393acfaf5b4
uysolanki <uysolanki@gmail.com>
2026-03-31 11:08:25 +0530

    transaction processing

--- src/day31/InsertPreparedStatementUsingBatch.java
+++ src/day31/InsertPreparedStatementUsingBatch.java
@@ -11,33 +11,37 @@ public class InsertPreparedStatementUsingBatch {
 	public static void main(String[] args) throws ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver");
 		try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pppdb", "root", "");
-				PreparedStatement ps = con.prepareStatement("insert into emp values(?,?,?)");) 
-		{
-			Scanner sc = new Scanner(System.in);
-			
-			for(int i=1;i<=3;i++)
-			{
-			System.out.println("Enter emp number"); //109-113
-			int a = sc.nextInt();
+				PreparedStatement ps = con.prepareStatement("insert into emp values(?,?,?)");
+				Scanner sc = new Scanner(System.in);) {
 
-			System.out.println("Enter emp name");
-			String b = sc.next();
+			con.setAutoCommit(false);
+			try {
+				for (int i = 1; i <= 3; i++) {
+					System.out.println("Enter emp number"); // 109-113
+					int a = sc.nextInt();
 
-			System.out.println("Enter emp salary");
-			double c = sc.nextDouble();
+					System.out.println("Enter emp name");
+					String b = sc.next();
 
-			ps.setInt(1, a);
-			ps.setString(2, b);
-			ps.setDouble(3, c);
-			ps.addBatch();
+					System.out.println("Enter emp salary");
+					double c = sc.nextDouble();
+
+					ps.setInt(1, a);
+					ps.setString(2, b);
+					ps.setDouble(3, c);
+					ps.addBatch();
+				}
+
+				int rows[] = ps.executeBatch();
+				con.commit();
+
+				System.out.println(rows.length + " Records Inserted");
+			} catch (Exception ex1) {
+				con.rollback();
+				System.out.println("Transaction rolled back!");
+				ex1.printStackTrace();
 			}
-			
-			int rows[] = ps.executeBatch();
-
-			System.out.println(rows.length + " Records Inserted");
-		} 
-		catch (Exception ex1) 
-		{
+		} catch (Exception ex1) {
 			ex1.printStackTrace();
 		}
 	}
